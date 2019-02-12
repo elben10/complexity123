cc7bba8110fd939cb14653918ee28e22de0dd554
vzx151 vzx151 <vzx151@ku.dk>
2019-02-12 09:22:05 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+docs_requirements = []
+
+installation_requirements = []
+
+setup_requirements = [
+    "pytest-runner"
+]
+
+test_requirements = [
+    "pytest",
+]
+
+setup(
+    name='complexity',
+    version='0.1.1',
+    url='https://github.com/AudreyRoy/complexity.git',
+    author='Audrey Roy',
+    author_email='audreyr@gmail.com',
+    description='Refreshingly simple static site generator.',
+    packages=find_packages(),    
+    install_requires=installation_requirements,
+    setup_requires=setup_requirements,
+    extras_require={
+        'docs': docs_requirements,
+        'test': test_requirements,
+    }
+)
